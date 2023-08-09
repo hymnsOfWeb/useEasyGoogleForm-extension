9418fb4d4182720293a6342d3f913f4e2ea143ba
theGobindSingh <thesinghgobind@gmail.com>
2023-08-10 00:33:26 +0530

    kaafi done

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,25 @@
 import typescript from "@rollup/plugin-typescript";
 import cjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
 /** @type {import('rollup').RollupOptions} */
 const themeConfig = {
   input: "src/back-end/index.ts",
   output: [
     {
-      file: "src/back-end/index.js",
+      file: "src/base/index.js",
       format: "cjs",
-      sourcemap: false
-    },
-    {
-      file: "src/back-end/index.mjs",
-      format: "esm",
       sourcemap: false
     }
   ],
   plugins: [
+    nodeResolve(),
     cjs({
       include: /node_modules/gm
     }),
-    typescript({ tsconfig: "./tsconfig.json" })
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser()
   ]
 };
 export default themeConfig;
--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -1,12 +1,73 @@
 import { Parser } from "htmlparser2";
-import str from "./main-str";
 
-const parse = new Parser({
-  onopentag(name, attribs) {
-    if (name === "input" && (attribs?.name ?? "").includes("entry.")) {
-      console.log(attribs.name);
+const fetcherFunc = async (url: string) => {
+  const fetchOne = await fetch(url);
+  const text = await fetchOne.text();
+  return text;
+};
+
+const parserer = async (html: string) => {
+  const entries: string[] = [];
+  const parse = new Parser({
+    onopentag(name, attribs) {
+      if (name === "input" && (attribs?.name ?? "").includes("entry.")) {
+        entries.push(attribs.name.replace("entry.", ""));
+      }
     }
+  });
+  parse.write(html);
+  parse.end();
+  return entries;
+};
+
+const getPrefillUrl = async (url: string) => {
+  const queryIndex = url.indexOf("?");
+  if (queryIndex !== -1) {
+    url = url.slice(0, queryIndex);
   }
-});
-parse.write(str);
-parse.end();
+  if (url.includes("/edit")) {
+    url = url.replace("/edit", "");
+  }
+  if (url[url.length - 1] !== "/") {
+    url += "/";
+  }
+  url += "prefill";
+  return url;
+};
+
+const getGFid = async (url: string) => {
+  const tempForIdOne = url.replace(/https:\/\/docs\.google\.com\/forms\/d(\/e)?\//gm, "");
+  const gFromId = tempForIdOne.slice(0, tempForIdOne.indexOf("/"));
+  return gFromId;
+};
+
+const createFinalString = async ({ gFromId, entries }: { gFromId: string; entries: string[] }) => {
+  let finalString = `useEasyGoogleForm({\nformRef: "" /** Your react form's ref */,\ngFormId: "${gFromId}",\nlinks: [\n`;
+  for (let i = 0; i < entries.length; i++) {
+    finalString += `{
+      entryId: "${entries[i]}",
+      formId: "" /** Your react form's input id */,
+    },`;
+  }
+  finalString += "]\n});";
+  return finalString;
+};
+
+const main = async () => {
+  const currentUrl = window.location.href;
+  const url = await getPrefillUrl(currentUrl);
+  const gFromId = await getGFid(currentUrl);
+  const html = await fetcherFunc(url);
+  const entries = await parserer(html);
+  const finalString = await createFinalString({ gFromId, entries });
+  const containerDiv = document.querySelector(".Ltcunf");
+  const oldHtml = containerDiv.innerHTML;
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("BdZlM");
+  newDiv.classList.add("useEasyGoogleForm");
+  newDiv.innerHTML = "<style>.useEasyGoogleForm{margin-top: 20px; padding: 5%;}</style>";
+  newDiv.innerHTML += `<span>${finalString}</span>`;
+  let newHtml = newDiv.outerHTML + oldHtml;
+  containerDiv.innerHTML = newHtml;
+};
+main();
